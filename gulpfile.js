854faeafcb459313cf2ffe1403166bc82c98ccfe
pannchat <pannchat@likelion.org>
2019-11-26 21:02:10 +0900

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
-const gulp = require('gulp');
-//걸프의존성
+const gulp = require('gulp');	//걸프의존성
+const babel = require('gulp-babel'); 
 gulp.task('defualt', function(){
 // 걸프작업물
+    //노드소스
+    gulp.src(" ")
 });
