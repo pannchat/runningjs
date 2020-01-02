bb3de8c55b594595c34140ffc85e49398267ddca
pannchat <pannchat@likelion.org>
2020-01-02 17:26:59 +0900

    3장

--- public/es6/test7.js
+++ public/es6/test7.js
@@ -1,3 +1,23 @@
 const numStr ="33.3";
 const num = Number(numStr);
 console.log(typeof num +" "+ num);
+
+const a = parseInt("16 volts", 10) // "volts는 무시되고 10진수 16
+const b = parseInt("3a",16); // 16진수 3a를 10진수로 => 58
+const c = parseFloat("15.5 kph");// parseFloat는 항상 기수가 10이라고 가정
+console.log(a);
+console.log(b);
+console.log(c);
+//시간
+const d = new Date();
+const ts = d.valueOf();
+console.log(ts);
+
+const times = new Date('2020-01-02T06:36:40.822Z');
+console.log(times.toString())
+
+//배열 toString객체사용
+const arr = [1,true,"hello"];
+const arrToString = arr.toString();
+console.log(arrToString);
+console.log(typeof arrToString);
---
+++ public/es6/test8.js
@@ -0,0 +1,34 @@
+// 참조형과 원시형
+let a = 1 ;
+let b = a;
+a = 2; // 원본의 값이 바뀌지만 사본의 값은 바뀌지 않음.
+console.log(a , b); // 2 1
+
+//함수로의 값 전달
+function change (c) {
+    c = 5
+}
+c = 3;
+change(c);
+console.log(c); //3
+
+let o ={a:1};
+let p = o; // p는 o가 가리키고 있는 것 을 가리키는것.
+o.a = 2;
+console.log(p); //{ a: 2 }
+
+let k = {a:1};
+let j = k; // j는 k가 가리키고 있는 것을 가리킴
+k === j; // true
+k = {a:2}; // k는 이제 다른것을 가리킴. {a:1}을 수정한 것이 아님.
+k === j; // false
+console.log(j);
+
+//참조 전달
+function change_x( x ){
+    x.a = 123;
+}
+
+let x = {a:1};
+change_x(x);
+console.log(x);
